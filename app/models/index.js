416e5825d5881bf719ab54939275c2a971d70aff
Saiq <141465128+xsaiq@users.noreply.github.com>
2023-12-08 23:07:55 +0700

    Delete app directory

--- app/models/index.js
+++
@@ -1,8 +0,0 @@
-const dbconfig = require("../config/database");
-const mongoose = require("mongoose");
-
-module.exports = {
-    mongoose,
-    url: dbconfig.url,
-    mahasiswa: require('./mahasiswa.model.js')(mongoose)
-}
